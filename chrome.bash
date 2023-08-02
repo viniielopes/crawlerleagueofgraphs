23754d68c169aa468eee7c62dedfd7ebe83305d4
Vinicius Lopes <viniielopes@live.com>
2023-08-01 21:57:31 -0300

    chore: changed chrome bash

--- chrome.bash
+++ chrome.bash
@@ -1,3 +1,3 @@
-RUN wget -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_112.0.5615.165-1_amd64.deb && \
+wget -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_112.0.5615.165-1_amd64.deb && \
     apt -y install /tmp/chrome.deb && \
     rm -rf /tmp/* /var/cache/apk/*
